6c32b7563d72968da42eaf2890090326805f85da
christoph-LSN <94443931+christoph-LSN@users.noreply.github.com>
2024-07-31 08:00:10 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+app = Flask(__name__)
+
+# Laden des trainierten Modells
+model_name = ''  # Dies sollte das Verzeichnis sein, in dem Ihr Modell liegt
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_input = request.json.get('message')
+    inputs = tokenizer.encode(user_input, return_tensors='pt')
+    outputs = model.generate(inputs, max_length=500)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return jsonify({'response': response})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
